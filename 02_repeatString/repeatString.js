ef0edfeba7586f404abb863390609fbc5ba716aa
Bruno Lima <brunofigueiredolima@gmail.com>
2023-01-12 22:01:44 -0300

    até o 6

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,11 @@
-const repeatString = function() {
+const repeatString = function(word, n) {
+    var res = "";
 
+    for(let i = 0; i < n; i++) {
+        res += word;
+    }
+
+    return n>=0?res:"ERROR";
 };
 
 // Do not edit below this line
--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,17 @@
 const removeFromArray = function() {
+    let args = Array.from(arguments);
 
+    let ar = arguments[0];
+
+    for(let i = 1; i < args.length; i++) {
+        let element = args[i];
+        let index = ar.indexOf(element);
+        if (index >= 0) {
+            ar.splice(index, 1);
+        }
+    }
+
+    return ar;
 };
 
 // Do not edit below this line
